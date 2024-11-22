b01c5c137a0b1a01d0e340da98882d2af22be9ae
MaxPastukhov15 <st131119@student.spbu.ru>
2024-11-22 12:42:05 +0300

    Update rotate_clockwise.hpp so the menu is correct now

--- rotate_clockwise.hpp
+++ rotate_clockwise.hpp
@@ -8,14 +8,12 @@
 std::vector<RGB> rotate90Clockwise(const std::vector<RGB>& data, int width, int height) {
     std::vector<RGB> rotated(height * width);
 
-    for (int y = 0; y < height; ++y) {
+for (int y = 0; y < height; ++y) {
         for (int x = 0; x < width; ++x) {
-            rotated[x * height + (height - y - 1)] = data[y * width + x];
+            rotated[(width - x - 1) * height + y] = data[y * width + x];
         }
     }
 
     return rotated;
 }
-
 #endif // ROTATE_CLOCKWISE_HPP
-
